7a537f2a8b8e6ebc2bbd96a0d6fd394cf3811310
liangyong <laozi9420>
2022-08-24 14:11:08 +0800

    first commit

---
+++ BEP20/token.sol
@@ -0,0 +1,16 @@
+﻿pragma solidity ^0.5.0;
+
+import "./BEP20.sol";
+import "./BEP20Detailed.sol";
+
+contract Token is BEP20, BEP20Detailed {
+
+   
+   /**
+    *  全称、简写、小数精度
+    *  铸币数量
+    */
+    constructor () public BEP20Detailed("Ruby", "Ruby", 6) {
+        _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
+    }
+}
